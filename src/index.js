5e92f7aaf3d37be528217194243785e20f7a872f
deucenn <panix.black@web.de>
2024-10-19 14:56:35 +0200

    Finished the menu functionality

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
-console.log("It works!")
+import "./styles.css";
+import { renderHome } from "./home";
+import { renderMenu } from "./menu";
+import { renderAbout } from "./about";
+import { renderFooter } from "./footer";
 
-import './styles.css';
-import { renderHome } from './home';
-import { renderFooter } from './footer';
+const homeBtn = document.getElementById("home-btn");
+const menuBtn = document.getElementById("menu-btn");
+const aboutBtn = document.getElementById("about-btn");
+const content = document.getElementById("content");
+
+homeBtn.addEventListener("click", () => {
+  content.innerHTML = "";
+  renderHome();
+  renderFooter();
+});
+
+menuBtn.addEventListener("click", () => {
+  content.innerHTML = "";
+  renderMenu();
+  renderFooter();
+});
+
+aboutBtn.addEventListener("click", () => {
+  content.innerHTML = "";
+  renderAbout();
+  renderFooter();
+});
 
 renderHome();
 renderFooter();
