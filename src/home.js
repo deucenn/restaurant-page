9e29e15c90fd4726eafd76c7d8a6fb3baadc748c
deucenn <panix.black@web.de>
2024-10-19 12:19:11 +0200

    Build home site

---
+++ src/home.js
@@ -0,0 +1,34 @@
+function renderHome() {
+    const content = document.getElementById('content');
+
+    const home = document.createElement('div');
+    content.appendChild(home);
+    home.classList.add('home');
+    
+    const greeting = document.createElement('h1');
+    greeting.textContent = 'Buongiorno';
+    greeting.classList.add('greeting');
+    home.appendChild(greeting);
+
+    const restaurantName = document.createElement('h1');
+    restaurantName.textContent = 'La Dolce Vita';
+    restaurantName.classList.add('restaurant-name');
+    home.appendChild(restaurantName);
+
+    const divider = document.createElement('div');
+    divider.classList.add('divider');
+    home.appendChild(divider);
+
+    const description = document.createElement('h2');
+    description.textContent = 'Authentic Neapolitan Pizza';
+    description.classList.add('description');
+    home.appendChild(description)
+
+    const viewMenu = document.createElement('button');
+    viewMenu.textContent = 'View Menu';
+    viewMenu.classList.add('view-menu');
+    viewMenu.setAttribute('id','menu-btn');
+    home.appendChild(viewMenu)
+}
+
+export { renderHome };
